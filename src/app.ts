3db66ec1b7e93784ce0417aaff0e4aa48c409585
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-12-18 20:59:24 +0600

    project setup with welcome

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('<h1>Welcome to RoleScript!</h1>');
 });
 
 export default app;
